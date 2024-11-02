cfbe27b272da3373691c070b0f592368acc110fd
Anuska <anuskasth2022@gmail.com>
2024-11-03 08:34:01 +1100

    refining js basics

---
+++ string.js
@@ -0,0 +1,8 @@
+let fullName = "Anuska" + " " + "Shrestha";
+console.log(fullName);
+
+console.log(
+  `Hello! my name is ${fullName}. I am refining my js knowledge here. `
+);
+
+const productName = new String(`Digitalization`);
