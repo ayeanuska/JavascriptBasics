4c2fa35d07a30062c735e94e9c2093e65eb1e792
Anuska <anuskasth2022@gmail.com>
2024-11-03 08:09:18 +1100

    reining js basics

---
+++ operations.js
@@ -0,0 +1,25 @@
+// a program to find the sum , difference , product and quotient of given two numbers
+
+//Binary Operations
+
+let num1 = 20;
+let num2 = 40;
+
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+let quotient = num2 / num1;
+
+console.log("The two numbers are " + num1 + " and " + num2 + ".");
+
+// Adding two Stings
+
+let str1 = "Anuska";
+let str2 = "Shrestha";
+
+console.log(str1 + str2);
+
+// complex Operations
+
+console.log("1" + 2); // 12
+console.log(2 + "1" + 2); //212
