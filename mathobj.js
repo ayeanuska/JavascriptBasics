56f8c184f221143e3cc478984612c69ac4d5d88d
Anuska <anuskasth2022@gmail.com>
2024-11-03 22:52:00 +1100

    random number math obj

--- mathobj.js
+++ mathobj.js
@@ -1,8 +1,23 @@
-//math objectt
+// //math objectt
 
-//const ans= Math.PI;   //( gives value of pi)
+// //const ans= Math.PI;   //( gives value of pi)
 
-Math.round(4.4); // gives rounds up value
+// Math.round(4.4); // gives rounds up value
 
-Math.pow(8, 2);
-const answer = Math.pow(3, 3);
+// Math.pow(8, 2);
+// const answer = Math.pow(3, 3); // gives first number to thr power of second number
+
+// const answer = Math.sqrt(64);
+// //gives square root of the given number
+
+// Math.ceil(4.9); // rounds up to 5 greatest
+// Math.floor(4.9); // rounds up to 4 least
+
+// Math.min(22, 20, 12, -22); // gives the smallest mnumber
+
+// Math.max; //gives max among the given
+
+const answer = Math.random();
+// gives random number from 0-0.9999
+
+console.log(answer);
