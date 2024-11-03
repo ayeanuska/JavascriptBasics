1f0e19654ea1eeed28188bac2a54665a4ec56d91
Anuska <anuskasth2022@gmail.com>
2024-11-03 19:12:18 +1100

    string basic

--- string.js
+++ string.js
@@ -6,3 +6,25 @@ console.log(
 );
 
 const productName = new String(`Digitalization`);
+
+console.log(productName);
+
+console.log(productName.length);
+console.log(productName.toUpperCase());
+console.log(productName.charAt(3));
+console.log(productName.indexOf("p"));
+const newProduct = productName.substring(0, 4); // "Head" wil be displayed
+console.log(newProduct);
+
+// from Start Index to the End index (exclusive)
+productName.slice(-3); // 'ion'
+productName.slice(-3, -1); // 'io'
+productName.slice(0, -1); // 'Digitalizatio'
+productName.slice(4, -1); // 'atio'
+
+const address = " Hornsby ";
+// console.log(address);
+console.log(address.trim());
+
+const url = " https://github.com/ayeanuska";
+console.log(url.includes("Anuska"));
