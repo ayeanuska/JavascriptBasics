318772f1af822a13b3ec3fc744cd8f52b4a32197
Anuska <anuskasth2022@gmail.com>
2024-11-04 11:02:14 +1100

    recurrsion

--- advancedJs/functionalprog.js
+++ advancedJs/functionalprog.js
@@ -8,18 +8,22 @@
 // 1. side effect
 
 // 2. PURE FUNCTION
-const add = (a, b) => {
+/* const add = (a, b) => {
   return a + b;
 };
 
 // or
 
-const add2 = (a, b) => a + b;
+const add2 = (a,  b) => a + b;
+*/
 
 // - less bugg code
 // - v small and scalable
 
 // 3. HIGH ORDER FUNCTION
+
+// takes one or more fuction as argument or returns a function as a result
+
 /*  const val = add(2, 4) + add(2, 4) + add(4, 4);
 console.log(val); */
 
@@ -34,16 +38,28 @@ const val = calculator(3, 2, add);
 console.log(val); */
 
 // add two item and finf to the power ..
-const adAndSquare = (a, b) => {
-  const ttl = a + b;
-  const power = (multiplier) => ttl ** multiplier;
-  return power;
+// const adAndSquare = (a, b) => {
+//   const ttl = a + b;
+//   const power = (multiplier) => ttl ** multiplier;
+//   return power;
 
-  const val = addAndSquare;
-  2, 3;
-  console.log(val());
-};
+// };
+//   const val = addAndSquare(2, 3);   // a,b = 2,3
+//  /* or
+//   const val =addAndSquare (2,3) (2);*/
+
+//   console.log(val(2));  // to the power 2
 
 // RECURSION
+// - a techniqque where a function calls it self
+
+// - must have base case to stop recursion and a recursive case
+
+const factorial = (n) => {
+  if (n < 1) return 1;
+  return n * factorial(n - 1);
+};
+
+console.log(factorial(5));
 
 // HOMEWORK
--- mathobj.js
+++ mathobj.js
@@ -1,23 +1,29 @@
 // //math objectt
 
-// //const ans= Math.PI;   //( gives value of pi)
+//const ans= Math.PI;   //( gives value of pi)
 
-// Math.round(4.4); // gives rounds up value
+Math.round(4.4); // gives rounds up value
 
-// Math.pow(8, 2);
+Math.pow(8, 2);
 // const answer = Math.pow(3, 3); // gives first number to thr power of second number
 
 // const answer = Math.sqrt(64);
-// //gives square root of the given number
+//gives square root of the given number
 
-// Math.ceil(4.9); // rounds up to 5 greatest
-// Math.floor(4.9); // rounds up to 4 least
+Math.ceil(4.9); // rounds up to 5 greatest
+Math.floor(4.9); // rounds up to 4 least
 
-// Math.min(22, 20, 12, -22); // gives the smallest mnumber
+Math.min(22, 20, 12, -22); // gives the smallest mnumber
 
-// Math.max; //gives max among the given
+Math.max; //gives max among the given
 
-const answer = Math.random();
+// const answer = Math.random();
 // gives random number from 0-0.9999
 
 console.log(answer);
+
+//generate a random number.
+const answer = Math.random() * 100;
+
+const val = Math.round(answer);
+console.log(val);
