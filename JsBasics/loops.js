4a4004cbf3352bf1cf0374ca70668dda32bace03
Anuska <anuskasth2022@gmail.com>
2024-11-10 16:05:47 +1100

    loops

---
+++ JsBasics/loops.js
@@ -0,0 +1,24 @@
+// for , while , do while are commonly used loops by most of the programming languages.
+
+// 1. for loop and 2. hile loop always chwcks the condition before any execution
+//do while runs the code once and then checks for conditiins
+
+// breakout loop
+// continue- help exit and skip one Time
+// break- stop /break or switch lopp.breakout
+
+for (let i = 0; i <= 100; i++) {
+  //     if (i==10)
+  //         break;
+  //     continue;
+
+  // }
+  // console.log(i);
+
+  // to find even Num.
+  if (i % 2 === 1) {
+    continue;
+  }
+  console.log(i);
+  // skips odd number
+}
