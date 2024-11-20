2c2bded0143177b3336288f6d9803a7d51aeb40c
Anuska <anuskasth2022@gmail.com>
2024-11-20 23:49:21 +1100

    new topics

---
+++ advancedJs/topics.js
@@ -0,0 +1,10 @@
+/* memorization: technique used to optimize functions by storing 
+the results of expensive function call */
+
+// currying?
+
+// why cdn instead of local CSS
+
+a; /*t times when servers are slow,
+are available in every region
+quicker */
