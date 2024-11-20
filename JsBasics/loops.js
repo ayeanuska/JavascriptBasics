f15abed5a4b8704b7719de6bf1d62f419294d880
Anuska <anuskasth2022@gmail.com>
2024-11-20 23:47:38 +1100

    array / loop

--- JsBasics/loops.js
+++ JsBasics/loops.js
@@ -7,18 +7,34 @@
 // continue- help exit and skip one Time
 // break- stop /break or switch lopp.breakout
 
-for (let i = 0; i <= 100; i++) {
-  //     if (i==10)
-  //         break;
-  //     continue;
+// for (let i = 0; i <= 100; i++) {
+//   //     if (i==10)
+//   //         break;
+//   //     continue;
 
-  // }
-  // console.log(i);
+//   // }
+//   // console.log(i);
 
-  // to find even Num.
-  if (i % 2 === 1) {
-    continue;
-  }
-  console.log(i);
-  // skips odd number
+//   // to find even Num.
+//   if (i % 2 === 1) {
+//     continue;
+//   }
+//   console.log(i);
+//   // skips odd number
+// }
+
+//Js specific array loops
+
+// for / of for (x of cars )  for array
+const fruits = ["apple", "banana", "orange", " mango"];
+console.log(fruits);
+
+const upperFruits = [];
+
+for (let item of fruits) {
+  // upperFruits.push(item.toUpperCase());
+  upperFruits.push(item.toUpperCase() + "-sku");
 }
+console.log(upperFruits);
+
+// to concat in one go
