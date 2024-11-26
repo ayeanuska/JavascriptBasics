ee677f7aae2f663612b7e2d4898a29d00cd6d52c
Anuska <anuskasth2022@gmail.com>
2024-11-26 22:42:28 +1100

    destructuring/ es6

--- JsBasics/objects.js
+++ JsBasics/objects.js
@@ -26,7 +26,7 @@ const person = {
   },
 };
 
-console.log(person);
+// console.log(person);
 
 // accesing data
 
@@ -36,18 +36,35 @@ console.log(person);
 // const data = person.skills[4][2],//double nested- double index- got flexbox
 // const data = person.parents.f;
 
-console.log(data);
+// console.log(data);
 
-//updating data
+// //updating data
 
-person.name = "ayeanuska";
+// person.name = "ayeanuska";
 
-console.log(person);
+// console.log(person);
 
 //adding data in obj is the same as updating
 // order doesnt matter in obj while matters for array
 
-(person.address = "pacific highway hornsby"),
-  (person.parents.bro = "anjish"),
-  person.skills[3].push("UI/Ux ");
-console.log(person);
+// (person.address = "pacific highway hornsby"),
+//   (person.parents.bro = "anjish"),
+//   person.skills[3].push("UI/Ux ");
+// console.log(person);
+
+// to delete data
+// (person.pets = null), // delete the data but still hold memory
+  ///can use undefined
+
+  // delete person.phone // not best way but dleetes both properties and data
+
+  // delete for array
+//   person.skills[2].pop();
+
+// delete person.parents.f
+
+
+//destructuring  es6 obj
+
+const { name, phone,age , parents} = person; 
+console.log(name, age parents:{f}); //anuska and undefined 
