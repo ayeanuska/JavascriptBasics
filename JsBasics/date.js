e51e8604fdaeefda909517c06ee5f3848d483041
Anuska <anuskasth2022@gmail.com>
2024-11-27 23:52:52 +1100

    revising date

--- JsBasics/date.js
+++ JsBasics/date.js
@@ -2,5 +2,22 @@
 
 // getComputedStyle, set, format
 
-const today = Date();
-console.log(today);
+// const today = Date(); //GOVE STRING
+// const today = new Date(); // GIVE  OBJ
+// console.log(today);
+
+// date is obj
+// Date()- constructor
+
+// const today = Date.now()
+
+//if you want to get specific time / daay/ date
+// const today = new Date();
+
+// const data = today.getDate()
+// const data = today.getDay()
+// const data = today.getFullYear()
+
+const today = new Date("2020-8-12");
+const data = today.getDay();
+console.log(data); // will give which day was it o that date
