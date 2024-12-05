96df4a3f48965501d54210a017648974591aa51f
Anuska <anuskasth2022@gmail.com>
2024-12-06 00:01:07 +1100

    es6 function

--- JsBasics/function.js
+++ JsBasics/function.js
@@ -1,32 +1,39 @@
 // Function help us to choose the codes that we want to run every time page loads
 
-const { func } = require("prop-types");
-
 // maintable and more simpler coding
 // 3 stages -
 // create func
 // call function
 // invoke function
 
-function sayHi(name, app, level = n / a) {
-  // level = .. is a default value .
-  const str =
-    " hello world this is " +
-    name +
-    " , revising function basiccs in " +
-    app +
-    "";
-  console.log(str);
-}
+// function sayHi(name, app, level = n / a) {
+//   // level = .. is a default value .
+//   const str =
+//     " hello world this is " +
+//     name +
+//     " , revising function basiccs in " +
+//     app +
+//     "";
+//   console.log(str);
+// }
 
-sayHi("anu , vscode"); // calling function and invoke
-sayHi("aye"); // dynaic using concat "+.+"
+// sayHi("anu , vscode"); // calling function and invoke
+// sayHi("aye"); // dynaic using concat "+.+"
 
-// ALTERANATE  template literals
+// // ALTERANATE  template literals
 
-function sayHi(name, level) {
+// function sayHi(name, level) {
+//   const str = `${name} lets use template literals `;
+//   console.log(str);
+// }
+
+// sayHi("anuska ");
+
+// ES6 arrow function
+const sayHi = (name, add = 23) => {
   const str = `${name} lets use template literals `;
   console.log(str);
-}
+};
+// sayHi("anuska ");
 
-sayHi("anuska ");
+sayHi;
