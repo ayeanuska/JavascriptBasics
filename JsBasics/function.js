2b169fb8144b71cf2ba157351f37827b204e7b07
Anuska <anuskasth2022@gmail.com>
2024-12-06 13:54:45 +1100

    destructuring function

--- JsBasics/function.js
+++ JsBasics/function.js
@@ -29,11 +29,39 @@
 
 // sayHi("anuska ");
 
-// ES6 arrow function
-const sayHi = (name, add = 23) => {
-  const str = `${name} lets use template literals `;
-  console.log(str);
-};
-// sayHi("anuska ");
+// // ES6 arrow function
+// const sayHi = (name, add = 23) => {
+//   const str = `${name} lets use template literals `;
+//   console.log(str);
+// };
+// // sayHi("anuska ");
 
-sayHi;
+// sayHi;
+
+// destructuring
+
+const userList = [
+  { name: "anu", location: "hornsby", intrests: " digital art" },
+
+  { name: "sam", location: "gordon", intrests: "music and guitar" },
+
+  { name: "ram", location: "gor", intrests: "mus guitar" },
+];
+
+console.log(userList);
+
+const personBio = ({ name, location, intrests }) => {
+  //   const { name, location, intrests } = person;
+  //   const str = ` ${person.name} is from ${person.location} and she loves ${person.intrests} `;
+  const str = ` ${name} is from ${location} and she loves ${intrests} `;
+  return str;
+};
+
+// const date = personBio();
+// console.log(data);
+
+// const aye= " aye loves creating digital art and is a ugc creator"
+userList.foreEach((item, i) => {
+  const data = personBio(item);
+  console.log(data);
+});
